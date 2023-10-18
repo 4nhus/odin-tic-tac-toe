a8fa6e8f443b702d18cfdf44363996d1d02a8a45
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-10-19 10:35:31 +1100

    Add logic to render board

--- game.js
+++ game.js
@@ -1,6 +1,26 @@
 let displayController = {
-    render: function() {
+    render: function(board) {
+        const grid = document.getElementById('grid');
+        grid.remove();
 
+        const newGrid = document.createElement('div');
+        newGrid.id = 'grid';
+
+        for (let row = 0; row < 3; row++) {
+            for (let column = 0; column < 3; column++) {
+                const cellButton = document.createElement('button');
+                const index = (row * 3) + column;
+                cellButton.textContent = board[index];
+                cellButton.addEventListener('click', () => {
+                    gameController.addMark(row, column);
+                });
+
+                newGrid.appendChild(cellButton);
+            }
+        }
+
+        const body = document.querySelector('body');
+        body.appendChild(newGrid);
     }
 }
 
@@ -11,24 +31,29 @@ let gameBoard = {
     addMark: function(playerSymbol, row, column) {
         const index = (row * 3) + column;
         this.board[index] = playerSymbol;
+        displayController.render(this.board)
     },
 
-    clearBoard: function() {
+    init: function() {
         this.board = [];
 
         for (let i = 0; i < 9; i++) {
             this.board.push('');
         }
 
-        displayController.render();
+        displayController.render(this.board);
     }
+}
+
+function playerFactory(playerSymbol) {
+    return {playerSymbol};
 }
 
 let gameController = {
     gameBoard: gameBoard,
     xPlayer: null,
     oPlayer: null,
-    currentPlayerSymbol: null,
+    currentPlayerSymbol: 'X',
 
     setXPlayer: function(player) {
         this.xPlayer = player;
@@ -43,15 +68,17 @@ let gameController = {
     },
 
     addMark: function(row, column) {
-        if (this.currentPlayer) {
+        if (this.currentPlayerSymbol) {
             gameBoard.addMark(this.currentPlayerSymbol, row, column);
         }
+    },
+
+    init: function() {
+        gameController.setXPlayer(playerFactory('X'));
+        gameController.setOPlayer(playerFactory('O'));
+        gameBoard.init();
     }
 }
 
-function playerFactory(playerSymbol) {
-    return {playerSymbol};
-}
+gameController.init();
 
-gameController.setXPlayer(playerFactory('X'));
-gameController.setOPlayer(playerFactory('O'));
